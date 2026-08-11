50289d9ed1a6cc8922486f9d9d750b960c5bf4ad
g66174437-cloud <gkorukonda6@gmail.com>
2026-08-11 12:06:12 +0530

    push sv1.0 before the merge

--- classes/class.sv
+++ classes/class.sv
@@ -1,7 +1,7 @@
 // Code your testbench here
 // or browse Examples
 //====================(local)================================
-class A;
+class A1;
   rand  local int unsigned a;
   rand local  int unsigned b;
   
